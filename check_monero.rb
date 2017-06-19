c2633114c96ab1bd7972227511b5dabd1475b473
Giancarlo Franchini <gfranchini@greathealthworks.com>
2017-06-19 17:19:06 -0400

    added views

--- check_monero.rb
+++ check_monero.rb
@@ -1,11 +1,16 @@
+require 'sinatra'
 require 'httparty'
 require 'nokogiri'
 require 'tty-spinner'
-require 'sinatra'
 require 'uri'
 
+#Pulling data from API
+spinner = TTY::Spinner.new("[:spinner] Gathering data ...", format: :classic)
 url = 'https://monerohash.com/api/stats_address?address=45f25xoWSUvPdvDXCQjmEv7ihEceXF6nsVeyNtbavtmFQPgoUAtnKpDgX5w4jHV9dLizRVXLEkuGwBnaTMjDFwq4NTwgkM4&longpoll=true'
-stats = HTTParty.get(url)
+stats = nil
+spinner.run('Done!') do
+  stats = HTTParty.get(url)
+end
 #Setting variables.
 balance = (stats['stats']['balance'].to_f)/1000000000000
 hash_rate=(stats['stats']['hashrate'])
@@ -15,6 +20,9 @@ dom = Nokogiri::HTML(price_stats.body)
 price = dom.css('.coinprice')[0].text
 price = price.gsub(/\r\n?/, "").delete(' ').tr('$','').to_f
 pending_balance = price * balance
+payments = (stats['payments'][0].split(":").map(&:strip)[1].to_f)/1000000000000
+
+
 
 set(:bind, "0.0.0.0")
 
@@ -22,5 +30,6 @@ get("/") do
   @balance = "#{balance}"
   @pending_balance = "#{pending_balance}"
   @hash_rate = "#{hash_rate}"
+  @payments = "#{payments}"
   erb(:index)
 end
