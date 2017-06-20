11b617e285e552855ec96fe6b5386cfc9db9feb2
Giancarlo Franchini <gfranchini@greathealthworks.com>
2017-06-20 19:19:37 -0400

    added last block found parameter

--- server.rb
+++ server.rb
@@ -3,11 +3,13 @@ require 'httparty'
 require 'nokogiri'
 require 'uri'
 
+
+
 class Monero
-  attr_reader(:url, :stats, :balance, :hash_rate, :price, :worth)
+  attr_reader(:stats, :balance, :hash_rate, :price, :worth, :lastBlockFound)
 
   def initialize
-    @url = 'https://monerohash.com/api/stats_address?address=45f25xoWSUvPdvDXCQjmEv7ihEceXF6nsVeyNtbavtmFQPgoUAtnKpDgX5w4jHV9dLizRVXLEkuGwBnaTMjDFwq4NTwgkM4&longpoll=true'
+    url = 'https://monerohash.com/api/stats_address?address=45f25xoWSUvPdvDXCQjmEv7ihEceXF6nsVeyNtbavtmFQPgoUAtnKpDgX5w4jHV9dLizRVXLEkuGwBnaTMjDFwq4NTwgkM4&longpoll=true'
     @stats = HTTParty.get(url)
     @balance = (@stats['stats']['balance'].to_f)/1000000000000
     @hash_rate = (@stats['stats']['hashrate'])
@@ -16,6 +18,13 @@ class Monero
     price = dom.css('.coinprice')[0].text
     @price = price.gsub(/\r\n?/, "").delete(' ').tr('$','').to_f
     @worth = @price * @balance
+
+    url2 = 'https://monerohash.com/api/stats'
+    response = HTTParty.get(url2, {headers: {'accept-encoding' => 'none'}})
+    lastBlockFound = response['pool']['lastBlockFound']
+    lastBlockFound = Time.strptime(lastBlockFound,'%Q').to_f.to_s
+    @lastBlockFound = DateTime.strptime(lastBlockFound, '%s')
+
   end
 end
 
@@ -26,5 +35,6 @@ get("/") do
   @balance = a.balance
   @worth = a.worth
   @hash_rate = a.hash_rate
+  @lastBlockFound = a.lastBlockFound
   erb(:index)
 end
